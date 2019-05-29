340f3c9c8ca1e54278178f0e77089e1e9c4cf8ca
Sean Yen <seanyen@microsoft.com>
2019-05-29 13:47:24 -0700

    Add Python DLLs path.

--- env_hook/setup.bat
+++ env_hook/setup.bat
@@ -1,1 +1,1 @@
-set "PATH=c:\opt\rosdeps\x64\bin;c:\opt\rosdeps\lib;c:\opt\python37amd64\Scripts;c:\opt\python37amd64\;%PATH%"
+set "PATH=c:\opt\python37amd64\Scripts;c:\opt\python37amd64\;c:\opt\python37amd64\DLLs;c:\opt\rosdeps\x64\bin;c:\opt\rosdeps\lib;%PATH%"
